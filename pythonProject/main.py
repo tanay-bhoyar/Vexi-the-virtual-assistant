449679b8eb844aa9c108e70ede63b4b5e6e0cab9
tanay-bhoyar <66437798+tanay-bhoyar@users.noreply.github.com>
2024-03-21 20:24:11 +0530

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -44,25 +44,25 @@ def sayJokes():
 
 def run():
     talk("what can i do for you")
-    command = takeCommand()
-    print(command)
-    if 'joke' in command:
+    text = takeCommand()
+    print(text)
+    if 'joke' in text:
         sayJokes()
-    elif 'play' in command:
-        song=command.replace('play',"")
+    elif 'play' in text:
+        song=text.replace('play',"")
         talk('playing'+song)
         pwk.playonyt(song)
-    elif 'time' in command:
+    elif 'time' in text:
         talk(datetime.now().strftime("%H:%M"))
-    elif 'date' in command:
+    elif 'date' in text:
         talk(date.today())
-    elif "who the heck is" in command:
-        person = command.replace('who the heck is','')
+    elif "who the heck is" in text:
+        person = text.replace('who the heck is','')
         info = wikipedia.summary(person,5)
         talk(info)
-    elif 'are you single' in command:
+    elif 'are you single' in text:
        talk("sorry i am in a relationship with wifi")
-    elif 'who are you' in command:
+    elif 'who are you' in text:
         talk("my name is vexi i am a virtual assistant made by K1NG")
     else:
         talk("say the command again")
